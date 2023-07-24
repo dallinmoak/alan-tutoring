570fa68a2fb9dbc36888fc86b0409cf0fcfee0a4
dallinmoak <dallinmoak@gmail.com>
2023-07-23 19:29:45 -0600

    tryina fix dynamic server error

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from 'next/headers'
+export const dynamic = 'force-dynamic';
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
