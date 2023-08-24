47e00d6613343adb20a50acfa2d2f763b29d9e82
dallinmoak <dallinmoak@gmail.com>
2023-08-23 23:18:27 -0600

    use the usePathname

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,14 @@
 "use client";
 
+import { usePathname } from 'next/navigation'
+
 export default function NotFound() {
 
+  const pathname = usePathname();
   return (
     <div className="flex flex-col items-center gap-1">
       <span className="bg-danger-darker dark:bg-danger- rounded p-[0.5em] text-light-shades- dark:text-dark-shades-">
-        404: Path {`{current path(TODO)}`} Not Found
+        404: Path {pathname} Not Found
       </span>
     </div>
   );
