4f0fd1096185a0d8d5222a1efc10828d957b1db0
dallinmoak <dallinmoak@gmail.com>
2023-08-23 23:03:15 -0600

    patch

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className="flex flex-col items-center gap-1">
       <span className="bg-danger-darker dark:bg-danger- rounded p-[0.5em] text-light-shades- dark:text-dark-shades-">
-        404: Path {window.location.pathname} Not Found
+        404: Path {`{current path(TODO)}`} Not Found
       </span>
     </div>
   );
