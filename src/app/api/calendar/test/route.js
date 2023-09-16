b12f41aadb9908c01e547d8a29e8bb0a7d7dc8ea
dallinmoak <dallinmoak@gmail.com>
2023-09-16 17:08:01 -0600

    test add google request

--- src/app/api/calendar/test/route.js
+++ src/app/api/calendar/test/route.js
@@ -14,5 +14,21 @@ export async function POST(request) {
     key: process.env.GOOGLE_SA_PRIVATE_KEY.replaceAll("\\n", "\n"),
     scopes: ["https://www.googleapis.com/auth/calendar"],
   });
-  return new Response(JSON.stringify({request: requestData, jwt: client}), {status: 201});
+  try {
+    const res = await client.request({
+      url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.GOOGLE_CALENDAR_ID}/events`,
+      method: "POST",
+      body: JSON.stringify(requestData),
+    });
+    return new Response(
+      JSON.stringify({ request: requestData, jwt: client, response: res }),
+      {
+        status: 201,
+      }
+    );
+  } catch (e) {
+    return new Response(JSON.stringify({ msg: "google error", e }), {
+      status: 501,
+    });
+  }
 }
