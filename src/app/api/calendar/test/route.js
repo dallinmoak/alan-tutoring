5bf511d388a45d58a26839f1cb9d0f0f42fc2267
dallinmoak <dallinmoak@gmail.com>
2023-09-16 17:01:45 -0600

    add jwt to test

--- src/app/api/calendar/test/route.js
+++ src/app/api/calendar/test/route.js
@@ -1,3 +1,7 @@
+import { JWT } from "google-auth-library";
+
+export const dynamic = "force-dynamic";
+
 export async function POST(request) {
   let requestData;
   try {
@@ -5,5 +9,10 @@ export async function POST(request) {
   } catch (e) {
     return new Response(JSON.stringify(e), { status: 501 });
   }
-  return new Response(JSON.stringify({request: requestData}), {status: 201});
+  const client = new JWT({
+    email: process.env.GOOGLE_SERVICE_ACCOUNT,
+    key: process.env.GOOGLE_SA_PRIVATE_KEY.replaceAll("\\n", "\n"),
+    scopes: ["https://www.googleapis.com/auth/calendar"],
+  });
+  return new Response(JSON.stringify({request: requestData, jwt: client}), {status: 201});
 }
